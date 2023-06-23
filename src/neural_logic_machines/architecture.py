6f6d9a9155c8e7ad6f7605d7872445a46c8ae4ad
Chloe <chloeajsilva@gmail.com>
2023-06-23 13:28:48 +0200

    implement select_body

--- src/neural_logic_machines/architecture.py
+++ src/neural_logic_machines/architecture.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+from itertools import permutations, chain, combinations
 import numpy as np
 
 class Architecture():
@@ -6,3 +6,8 @@ class Architecture():
     def permute(self, input):
         perm = permutations(range(input.ndim))
         return [np.transpose(input, p) for p in perm]
+
+    def select_body(self, input):
+        l = list(range(len(input)))
+        comb = chain.from_iterable(combinations(l, r) for r in range(len(l)+1))
+        return [np.array([input[j] for j in i]) for i in comb]
--- src/unit_tests/architecture_tests.py
+++ src/unit_tests/architecture_tests.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import collections
 import neural_logic_machines.architecture as architecture
 
 class TestArchitecture(unittest.TestCase):
@@ -37,6 +38,53 @@ class TestArchitecture(unittest.TestCase):
                         [0, 0, 1, 0],
                         [1, 0, 0, 0],
                         [0, 0, 0, 1]]])).all())
+        
+    def test_select_body_2_unary_predicates(self):
+        # Given
+        input = np.array([[0, 0, 1, 0],
+                          [0, 1, 0, 0]])
+
+        # When
+        output = self.arch.select_body(input)
+
+        # Then
+        self.assertTrue(self.same_np_arrays(output, [
+            np.array([[]]),
+            np.array([[0, 0, 1, 0]]),
+            np.array([[0, 1, 0, 0]]),
+            np.array([[0, 0, 1, 0],
+                      [0, 1, 0, 0]])]))
+
+    def test_select_body_3_binary_predicates(self):
+        # Given
+        input = np.array([[[0, 0], [0, 0]],
+                          [[0, 1], [1, 0]],
+                          [[1, 1], [1, 1]]])
+        
+        # When
+        output = self.arch.select_body(input)
+
+        # Then
+        self.assertTrue(self.same_np_arrays(output, [
+            np.array([[[]]]),
+            np.array([[[0, 0], [0, 0]]]),
+            np.array([[[0, 1], [1, 0]]]),
+            np.array([[[1, 1], [1, 1]]]),
+            np.array([[[0, 0], [0, 0]],
+                      [[0, 1], [1, 0]]]),
+            np.array([[[0, 0], [0, 0]],
+                      [[1, 1], [1, 1]]]),
+            np.array([[[0, 1], [1, 0]],
+                      [[1, 1], [1, 1]]]),
+            np.array([[[0, 0], [0, 0]],
+                      [[0, 1], [1, 0]],
+                      [[1, 1], [1, 1]]])]))
+
+    def same_np_arrays(self, x, y):
+        if len(x) != len(y):
+            return False
+        
+        return all([any([np.array_equiv(a1, a2) for a2 in y]) for a1 in x])
 
 if __name__ == '__main__':
     unittest.main()
